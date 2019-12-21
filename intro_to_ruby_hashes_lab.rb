0e2655d6642cb9985003117ce4c8cc32cfbee9a5
Adam  Shappy <ajs11300@gmail.com>
2019-12-21 19:34:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,11 +27,12 @@ def monopoly_with_third_tier
 	  :railroads => {
 	    :pieces => 4,
 	    :rent_in_dollars => {
-	      :names => {},
+	      :random => {},
 	      :random2 => {},
 	      :random3 => {},
 	      :random4 => {},
-	    }
+	    },
+	    :names => {}
 	  }
 	}
 end
